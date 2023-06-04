34c23848cc50be7b31e602fe9bf250dadc78709a
markow02 <mkowalczyk1213@gmail.com>
2023-06-04 23:06:32 +0200

    ‎

---
+++ 10_ostatnia_cyfra.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_ostatnia_cyfra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę naturalną");
+            int number = inputValue();
+
+            int ostatniaCyfra = number % 10;
+
+            Console.WriteLine("Ostatnią cyfrą " + number + " jest: " + ostatniaCyfra);
+        }
+        private static int inputValue()
+        {
+            int result;
+            while (!int.TryParse(Console.ReadLine(), out result)) ;
+            return result;
+        }
+    }
+}
---
+++ 9_zamiana_miejsc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_zamiana_miejsc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("a -> b, b -> c, c -> a\r\n");
+
+          
+            double a, b, c;
+            Console.WriteLine("Podaj liczbe a");
+            a = inputValue();
+            Console.WriteLine("Podaj liczbe b");
+            b = inputValue();
+            Console.WriteLine("Podaj liczbe c");
+            c = inputValue();
+
+            a = a + b + c;
+            b = a - (b + c);
+            c = a - (b + c);
+            a = a - (b + c);
+
+            Console.WriteLine("\r\na: " + a);
+            Console.WriteLine("b: " + b);
+            Console.WriteLine("c: " + c);
+        }
+        private static double inputValue()
+        {
+            double result;
+            while (!double.TryParse(Console.ReadLine(), out result)) ;
+            return result;
+        }
+    }
+
+}
