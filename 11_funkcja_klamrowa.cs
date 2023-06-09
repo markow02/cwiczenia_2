20c8de269525b81e9c8f61f4075222c7891cb5d2
markow02 <mkowalczyk1213@gmail.com>
2023-06-09 20:14:15 +0200

    Create 11_funkcja_klamrowa.cs

---
+++ 11_funkcja_klamrowa.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_funkcja_klamrowa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static double FunkcjaKlamrowa(double x)
+        {
+            if (x <= 1.0)
+            {
+                if (x >= -2.0)
+                {
+                    return 0.0;
+                }
+                else
+                {
+                    return -1.0;
+                }
+            }
+            else
+            {
+                if (x <= 2.0)
+                {
+                    return 1.0;
+                }
+                else
+                {
+                    return 0.0;
+                }
+            }
+        }
+
+    }
+}
